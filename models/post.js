b5df4b969dd43d87e0cf0596a431d34d6fa7a8f1
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-13 14:21:56 -0400

    Created Schema

--- models/post.js
+++ models/post.js
@@ -1,1 +1,15 @@
-/// schema goes here
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Post = new Schema(
+    {
+        title: { type: String, required: true },
+        author: { type: String, required: true },
+        imgURL: { type: String, required: true },
+        content: { type: String, required: true },
+        date: { type: String, required: true },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('posts', Post)
