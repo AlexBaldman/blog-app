2093557e297f4234752559e11ff4a8cbec44cdf0
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-13 15:12:12 -0400

    finish populating objects

--- seed/posts.js
+++ seed/posts.js
@@ -29,29 +29,29 @@ const main = async () => {
             },
             {
                 "title": "Post 04",
-                "author": "",
-                "imgURL": "",
+                "author": "E. X. Press",
+                "imgURL": "https://images.unsplash.com/photo-1568367756922-5bf1c7d21cb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "10-20-19"
             },
             {
                 "title": "Post 05",
-                "author": "",
-                "imgURL": "",
+                "author": "Gi T. Hub",
+                "imgURL": "https://images.unsplash.com/photo-1559628151-ef85aab5bb21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "09-20-19"
             },
             {
                 "title": "Post 06",
-                "author": "",
-                "imgURL": "",
+                "author": "Bo O. T. Strap",
+                "imgURL": "https://images.unsplash.com/photo-1562613395-6864116cf26f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "08-20-19"
             },
             {
                 "title": "Post 07",
-                "author": "",
-                "imgURL": "",
+                "author": "R. E. Act",
+                "imgURL": "https://images.unsplash.com/photo-1567304286456-7e13b9456c27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "07-20-19"
             },
@@ -64,15 +64,15 @@ const main = async () => {
             },
             {
                 "title": "Post 09",
-                "author": "",
-                "imgURL": "",
+                "author": "J. Script",
+                "imgURL": "https://images.unsplash.com/photo-1555117899-1f51e50a94be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1190&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "05-20-19"
             },
             {
                 "title": "Post 10",
-                "author": "",
-                "imgURL": "",
+                "author": "Ter-min Al",
+                "imgURL": "https://images.unsplash.com/photo-1563304606-20d78cce7184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80",
                 "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 "date": "04-20-19"
             }
