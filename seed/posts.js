55bce9a51e749a5313788760b5c0fac2b116af5b
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-13 14:43:25 -0400

    Created posts array

--- seed/posts.js
+++ seed/posts.js
@@ -1,1 +1,89 @@
-// post JSON goes here.
+const db = require('../db/connection')
+const Post = require('../models/post')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const posts =
+        [
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            },
+            {
+                "title": "",
+                "author": "",
+                "imgURL": "",
+                "content": "",
+                "date": ""
+            }
+        ]
+    await Post.insertMany(posts)
+    console.log("Created posts.")
+}
+
+const run = async () => {
+    await main()
+    db.close()
+}
+
+run()
