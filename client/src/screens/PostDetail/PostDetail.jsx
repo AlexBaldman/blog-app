1c46e4cff6ac3cf5e9238baaff1c3e37968ce807
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-14 13:27:17 -0400

    Create constructor and componentDidMount

--- client/src/screens/PostDetail/PostDetail.jsx
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -6,9 +6,26 @@ import { getPost, deletePost } from '../../services/posts'
 import './PostDetail.css'
 
 export default class PostDetail extends Component {
-    constructor(props)
+    constructor(props) {
+        super(props)
+        this.state = {
+            post: {
+                title: '',
+                author: '',
+                content: '',
+                imgURL: ''
+            }
+        }
+    }
+
+    async componentDidMount() {
+        let { id } = this.props.match.params
+        const post = await getPost(id)
+        this.setState({ post })
+    }
 
     render() {
+        const { post } = this.state
         return (
             <Layout>
                 <div>
