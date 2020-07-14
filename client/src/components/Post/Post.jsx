052367dfd99c3a139d6df060367f283dfd270c23
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-14 15:52:11 -0400

    Update variables

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -6,8 +6,8 @@ export default class Post extends Component {
     render() {
         return (
             <>
-                <Link className='post' to={`/posts/${posts._id}`}>
-                    <img className='post-img' scr={posts.imgURL} />
+                <Link className='post' to={`/posts/${props._id}`}>
+                    <img className='post-img' scr={props.imgURL} />
                     <div className='post-date'>{props.date}</div>
                     <div className='post-title'>{props.title}</div>
                     <div className='post-author'>{props.author}</div>
