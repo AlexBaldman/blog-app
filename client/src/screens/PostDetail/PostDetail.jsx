9ed22be25e84c4c67c7e048387ff52964c0f37bf
Nocturnal <NocturnalMusing@users.noreply.github.com>
2020-07-14 13:23:45 -0400

    Create react class component

---
+++ client/src/screens/PostDetail/PostDetail.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Layout from '../../components/shared/Layout/Layout'
+import { getPost, deletePost } from '../../services/posts'
+
+import './PostDetail.css'
+
+export default class PostDetail extends Component {
+    constructor(props)
+
+    render() {
+        return (
+            <Layout>
+                <div>
+                </div>
+            </Layout>
+        )
+    }
+}
